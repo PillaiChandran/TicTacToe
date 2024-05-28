31e7b68b4a332da4b3f106c9fcc11b39fa787553
PillaiChandran <chandranpillai76@gmail.com>
2024-05-28 23:59:00 +0530

    React Game

---
+++ src/TicTacToe/Square.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Square(props){
+  
+    return( 
+
+        <div className="square"  onClick={props.onClick}>
+          
+            <h1>{props.value}</h1>
+        </div>
+    )
+}
+export default Square;
